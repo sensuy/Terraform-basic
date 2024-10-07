be63955971e24b2fb4da8c276f821e02509bb338
Sensui <sensuijunior@hotmail.com>
2024-10-07 10:10:44 -0300

    base completed

--- local.tf
+++ local.tf
@@ -1,4 +1,32 @@
 resource "local_file" "exemplo" {
     filename = "exemplo.txt"
-    content = "Fullcycle"
+    content = var.conteudo
+}
+
+data "local_file" "conteudo_exemplo" {
+  filename = "exemplo.txt"
+}
+
+output "data-source-result" {
+  value = data.local_file.conteudo_exemplo.content
+  
+}
+
+variable "conteudo" {
+    type = string
+    default = "Olá, Mundo!"
+}
+
+output "id-do-arquivo" {
+    value = resource.local_file.exemplo.id
+  
+}
+
+output "nome-do-arquivo" {
+    value = resource.local_file.exemplo.filename
+  
+}
+
+output "chiken-egg" {
+    value = sort(["🐔", "🥚"])
 }
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+conteudo = "valor do output"
