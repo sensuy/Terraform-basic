f7d539df7439df63c74d4afacdef0991f61433e6
Sensui <sensuijunior@hotmail.com>
2024-12-02 22:57:53 -0300

    finished terraform introduction

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_version = "1.9.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.77.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+
+    backend "s3" {
+        bucket = "myfcbucket"
+        key    = "terraform.tfstate"
+        region = "us-east-2"
+    }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
 module "new-vpc" {
     source = "./modules/vpc"
     prefix = var.prefix
